f6f26ed5c5569ec7b910ea54c010cd81a9b1bce4
hduperier <59696097+hduperier@users.noreply.github.com>
2022-10-04 12:40:42 -0400

    Add files via upload

---
+++ A3/pre_mkd_a3.R
@@ -0,0 +1,16 @@
+## Part 1
+  ## Question 2
+  print("2a). The lasso, relative to least squares, is less flexible and hence will give improved prediction accuracy when its increase in bias is less than its decrease in variance which proves iii to be correct since the lasso will have better predictions as bias increrases and variances decreases or remains the same.")
+  print("2b). The ridge regression, relative to least squares, is less flexible and hence will give improved prediction accuracy when its increase in bias is less than its decrease in variance which proves iii to be correct since the ridge regression will have better predictions as bias increrases and variances decreases or remains the same, similar to the lasso.")
+  print("2c. Non-linear methods, relative to least squares, are ")
+  ## Question 3
+  ## Question 10
+  ## Question 11
+## Part 2
+  ## Question 4
+  ## Question 8
+  ## Question 11
+  ## Question 12
+  ## Question 13
+## Part 3: Hockey Regression
+
